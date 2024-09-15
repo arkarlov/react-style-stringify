9f92658fab82e00f98c733d365d8cbb7cf2bfc80
Artem Karlov <ar.karlov@gmail.com>
2024-09-16 00:07:41 +0200

    doc: Add jsDoc

--- src/stringifyCSSProperties.ts
+++ src/stringifyCSSProperties.ts
@@ -1,6 +1,14 @@
 import { type CSSProperties } from "react";
 import { applyCssUnits, camelToKebab } from "./utils";
 
+/**
+ * Converts a CSSProperties object into a CSS string.
+ *
+ * @param {CSSProperties} cssProperties - An object representing the CSS properties, where the keys are camelCased property names and the values are the corresponding CSS values.
+ * @param {boolean} [isImportant=false] - A flag indicating whether to append the `!important` statement to each CSS property. Defaults to `false`.
+ *
+ * @returns {string} - A formatted CSS string, with properties converted to kebab-case and units added where necessary. If `isImportant` is true, each property will be suffixed with `!important`.
+ */
 export function stringifyCSSProperties(
   cssProperties: CSSProperties,
   isImportant: boolean = false
